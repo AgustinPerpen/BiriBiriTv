91ef59cada0c7870904d00ae72265545d966e304
imaalvrz <imanol.alvarez1@gmail.com>
2022-10-29 20:20:32 -0300

    responsive landing part1

--- biribiritv/src/App.js
+++ biribiritv/src/App.js
@@ -1,16 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import Header from './Components/Header'
-import Contacto from './Components/Contacto'
-import Programas from './Components/Programas';
-import Landing from './Components/Landing';
-=======
-
 import Header from './components/Header'
 import Contacto from './components/Contacto'
 import Programas from './components/Programas';
->>>>>>> 3fd10eb76187444cda2f3a4afa472ce8caeca805
+import Landing from './components/Landing';
 
 function App() {
       
--- biribiritv/src/Components/Landing/index.jsx
+++ biribiritv/src/Components/Landing/index.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Logo from '../../Assets/LandingImage.png'
-import './landing.css'
+import React from "react";
+
+import "./landing.css";
+
+import Logo from "../../assets/LandingImage.png";
+
+import Grid from "@mui/material/Grid";
 
 const Landing = () => {
   return (
-    <div className='landing-container'>
-        <div className='landing-info'>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit fusce litora, et molestie viverra ullamcorper suspendisse tincidunt montes posuere accumsan sem, massa tempus elementum ac conubia etiam morbi efficitur. Luctus eu convallis viverra imperdiet inceptos a efficitur arcu, vehicula nulla aptent sagittis suspendisse per fames dolor, parturient mattis rutrum donec condimentum eros semper. Pellentesque ipsum a ut finibus nullam potenti erat penatibus ac tristique taciti consectetur odio, ultrices arcu viverra ultricies tempor lorem lobortis ornare elementum pretium netus. Maximus interdum cubilia sapien vehicula suspendisse auctor imperdiet nec blandit, non facilisi platea nibh in vivamus varius euismod donec venenatis, eleifend parturient sagittis pharetra hendrerit odio aliquet ornare. Posuere neque lacinia risus finibus eu leo cras lobortis nisl vitae pulvinar consectetur conubia torquent class aliquam dictum, maecenas nostra cubilia nisi senectus id eget feugiat suspendisse mollis
-        </div>
-        <div className='landing-logo'>
-            <img src={Logo}/>
-        </div>
+    <div className="main">
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={4}>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit fusce litora,
+            et molestie viverra ullamcorper suspendisse tincidunt montes posuere
+            accumsan sem, massa tempus elementum ac conubia etiam morbi
+            efficitur. Luctus eu convallis viverra imperdiet...
+          </div>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <img src={Logo} className="left-img" />
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
