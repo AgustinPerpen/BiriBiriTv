30f997a058762e00a27024db39e8ea9aa5d88791
AgustinPerpen <agustinperpen@hotmail.com>
2022-10-29 17:46:36 -0300

    AP - Added Logo in Header

--- biribiritv/src/Components/Header/index.jsx
+++ biribiritv/src/Components/Header/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Logo from '../../Assets/HeaderLogo.png'
 
 const Header = () => {
   return (
     <div>
+        <img src={Logo} />
         <nav>
             Home
         </nav>
