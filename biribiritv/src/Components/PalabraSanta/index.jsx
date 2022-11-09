8db5ed7de8257ff3f8983ffeb992dd15fd74c320
AgustinPerpen <agustinperpen@hotmail.com>
2022-11-09 20:36:20 -0300

    AP - Added Palabrasanta images

--- biribiritv/src/Components/PalabraSanta/index.jsx
+++ biribiritv/src/Components/PalabraSanta/index.jsx
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 import "../Landing/landing.scss";
 
 import PalabraSantaLogo from '../../assets/PalabraSantaLogo.png'
+import Vivo from '../../assets/Vivo.jpg'
+import Horarios from '../../assets/Horarios.png'
 
 const PalabraSanta = () => {
   return (
-    <div>
-        
+    <div>            
         <div className="landing__subContainer">
         <Grid container spacing={2} alignItems="center">
           <Grid item xs={12} md={4}>
@@ -18,15 +19,31 @@ const PalabraSanta = () => {
               litora, et molestie viverra ullamcorper suspendisse tincidunt
               montes posuere accumsan sem...
             </div>
-            <div>
-              <Link to="/palabra-santa"><button>Ver detalles</button></Link>
-            </div>
           </Grid>
           <Grid item xs={12} md={8}>
             <img src={PalabraSantaLogo} className="landing__subContainer-img" />
           </Grid>
         </Grid>
-      </div>      
+      </div>
+      <div className="landing__subContainer">
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} md={8}>
+            <img
+              src={Vivo}
+              className="landing__subContainer-img landing__twitch-ps-img"
+            />
+                    
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <div className="landing__subContainer-info">
+                <img
+                src={Horarios}
+                className="landing__subContainer-img landing__twitch-ps-img"
+                />   
+            </div>
+          </Grid>
+        </Grid>
+      </div>    
     </div>
   )
 }
