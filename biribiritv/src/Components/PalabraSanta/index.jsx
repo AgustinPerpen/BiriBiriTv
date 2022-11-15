429abf22014790189f255d8a40b0e4c2f7aabddf
AgustinPerpen <agustinperpen@hotmail.com>
2022-11-15 18:37:41 -0300

    AP - Added Hover with info in palabraSanta imgs

--- biribiritv/src/Components/PalabraSanta/index.jsx
+++ biribiritv/src/Components/PalabraSanta/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import FooterPS from "../FooterPS";
@@ -7,13 +7,40 @@ import FooterPS from "../FooterPS";
 import "../PalabraSanta/palabraSanta.scss"
 
 import PalabraSantaLogo from '../../assets/PalabraSantaLogo.png'
-import Vivo from '../../assets/Vivo.jpg'
-import Horarios from '../../assets/horariosTrans.png'
 import Uno from '../../assets/uno.jpg'
 import Dos from '../../assets/dos.jpg'
 import Tres from '../../assets/tres.jpg'
 
 const PalabraSanta = () => {
+
+  const [infoOne, setInfoOne] = useState("")
+  const [infoTwo, setInfoTwo] = useState("")
+  const [infoThree, setInfoThree] = useState("")
+
+  const infoOneHandler = () => {
+    setInfoOne("ONE ONE ONE ONE")
+  }
+
+  const infoTwoHandler = () => {
+    setInfoTwo("TWO TWO TWO TWO")
+  }
+
+  const infoThreeHandler = () => {
+    setInfoThree("THREE THREE THREE THREE")
+  }
+
+  const infoOneRemove = () => {
+    setInfoOne("")
+  }
+
+  const infoTwoRemove = () => {
+    setInfoTwo("")
+  }
+
+  const infoThreeRemove = () => {
+    setInfoThree("")
+  }
+
   return (
     <div className='palabra-santa'>            
         <div className="palabra-santa__subContainer">
@@ -31,16 +58,18 @@ const PalabraSanta = () => {
           </Grid>
         </div>
         <div className="palabra-santa__photo">
-            <Grid container spacing={2} alignItems="center">
-              <Grid item xs={12} md={4}>
-                <img src={Uno} className="palabra-santa__box-img" />
-                <p className='palabra-santa__photo-info'>Tu vieja en tanga</p>
+            <Grid container spacing={3} alignItems="center">
+              <Grid  className="palabra-santa__photo-one" item xs={12} md={4}>
+                <img src={Uno} onMouseEnter={infoOneHandler} onMouseLeave={infoOneRemove} className="palabra-santa__box-img" />
+                <div className="palabra-santa__info-one">{infoOne}</div>              
               </Grid>
-              <Grid item xs={12} md={4}>
-                <img src={Dos} className="palabra-santa__box-img" />
+              <Grid className="palabra-santa__photo-two" item xs={12} md={4}>
+                <img src={Dos} onMouseEnter={infoTwoHandler} onMouseLeave={infoTwoRemove} className="palabra-santa__box-img" />
+                <div className="palabra-santa__info-two">{infoTwo}</div>
               </Grid>
-              <Grid item xs={12} md={4}>
-                <img src={Tres} className="palabra-santa__box-img" />
+              <Grid className="palabra-santa__photo-three" item xs={12} md={4}>
+                <img src={Tres} onMouseEnter={infoThreeHandler} onMouseLeave={infoThreeRemove} className="palabra-santa__box-img" />
+                <div className="palabra-santa__info-three">{infoThree}</div>
               </Grid>          
             </Grid>
         </div>
