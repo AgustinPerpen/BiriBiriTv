ab47c350e1accfe892a90afc3e18866e2afe7bee
AgustinPerpen <agustinperpen@hotmail.com>
2023-04-30 16:11:34 -0300

    created states for footer info

--- biribiritv/src/Components/FooterPS/index.jsx
+++ biribiritv/src/Components/FooterPS/index.jsx
@@ -6,19 +6,29 @@ import FooterImage from "../../Assets/FooterImage.png";
 import InstagramIcon from "../../Assets/icons/instagram.png";
 import TikTokIcon from "../../Assets/icons/tik-tok.png";
 import TwitterIcon from "../../Assets/icons/twitter.png";
+import YouTubeIcon from "../../Assets/icons/youtube.png";
 
 const FooterPS = () => {
 
-  
-  const [footerInfo, setFooterInfo] = useState(false)
+  const [email, setEmail] = useState("")
+  const [instagram, setInstagram] = useState("")
+  const [tikTok, setTikTok] = useState("")
+  const [twitter, setTwitter] = useState("")
+  const [psActive, setPsActive] = useState(true)
 
 
   useEffect(() =>{
 
     if(`${window.location.pathname}` === '/palabra-santa'){
-      setFooterInfo(true)
-    }else if(`${window.location.pathname}` === '/programas'){
-      setFooterInfo(false)
+      setEmail("palabrasanata.contacto@gmail.com")
+      setInstagram("https://www.instagram.com/palabrasantaok")
+      setTikTok("https://www.tiktok.com/@palabrasantaok")
+      setTwitter("https://www.twitter.com/palabrasantaok")
+    }else {
+      setEmail("quefla.contacto@gmail.com")
+      setInstagram("https://www.instagram.com/quefla_tv/")
+      setTwitter("https://twitter.com/quefla_tv")
+      setPsActive(false)
     }
   },[])  
 
@@ -27,50 +37,35 @@ const FooterPS = () => {
       <Grid container alignItems="center">
         <Grid item xs={12} md={4}>
           <img src={FooterImage} className="footer-PS__img"/>
-        </Grid>
-        <div>
-          {
-            footerInfo
-            ?
+        </Grid>          
           <Grid item xs={12} md={4}>          
               <div className="footer-PS__contacto-box">
                 <span className="title">Contactanos:</span>
-                <span> palabrasanata.contacto@gmail.com</span>
+                <span> {email} </span>
                 <span> 341-7121552</span>
               </div>          
-          </Grid>
-          :
-          <div>
-            <Grid item xs={12} md={4}>
-            <img src={FooterImage} className="footer-BB__img" />
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <div className="footer-BB__contacto-box">
-              <span className="title">Contactanos:</span>
-              <span> quefla.contacto@gmail.com</span>
-              <span> 341-7121552</span>
-            </div>
-          </Grid>
-          </div>
-
-          }
-          </div>                 
+        </Grid>                          
         <Grid item xs={12} md={4}>
           <div className="footer-PS__box-redes">
             <>
-              <img src={InstagramIcon} className="footer-PS__link-icon"></img>
-              <a href="https://www.instagram.com/palabrasantaok">Instagram</a>
+              <img src={YouTubeIcon} className="footer-BB__link-icon"></img>
+              <a href={"https://www.youtube.com/@Quefla_Tv"}>
+                Youtube
+              </a>
             </>
             <>
+              <img src={InstagramIcon} className="footer-PS__link-icon"></img>
+              <a href={instagram}>Instagram</a>
+            </>
+            <>        
               <img src={TikTokIcon} className="footer-PS__link-icon"></img>
-              <a href="https://www.tiktok.com/@palabrasantaok">TikTok</a>
+              <a href={instagram}>TikTok</a>
             </>
           </div>
           <div className="footer-PS__box-redes">
             <>
               <img src={TwitterIcon} className="footer-PS__link-icon"></img>
-              <a href="https://www.twitter.com/palabrasantaok">Twitter</a>
+              <a href={twitter}>Twitter</a>
             </>
           </div>
         </Grid>        
